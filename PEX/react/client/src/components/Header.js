7df8e41d490301758be5cba8bf5c442f0c5eaf1d
Robert-025 <robert.sena25@gmail.com>
2025-05-16 17:54:28 -0300

    inicio navbar

--- PEX/react/client/src/components/Header.js
+++ PEX/react/client/src/components/Header.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import Navbar from './Navbar.js';
 
 import '../styles/App.css';
 import Logo from '../img/logo.png'
 
 export default function Header() {
     return (
-        <>
-            <header>
-                <div className="content-cabecalho">
-                    <div className="esquerdo-header">
-                        <img src={Logo} alt="Logo do site" />
-                    </div>
-                    <div className="direito-header">
-                        <nav className="links-cabecalho">
-                            <a href="/">Início</a>
-                            <a href="/quiz">Quiz</a>
-                            <a href="/about">Sobre</a>
-                        </nav>
-                    </div>
-                </div>
-            </header>
-        </>
+        // <header>
+        //     <div className="content-cabecalho">
+        //         <div className="esquerdo-header">
+        //             <img src={Logo} alt="Logo do site" />
+        //         </div>
+        //         <div className="direito-header">
+        //             <nav className="links-cabecalho">
+        //                 <a href="/">Início</a>
+        //                 <a href="/quiz">Quiz</a>
+        //                 <a href="/about">Sobre</a>
+        //             </nav>
+        //         </div>
+        //     </div>
+        // </header>
+        <React.Fragment>
+            <Navbar />
+        </React.Fragment>
     )
 }
 
